b9595f4c8da50cc1dbfdb5c0f7afada2047fc580
John (Jef) Fisher <Fishe545@gmail.com>
2017-03-06 11:12:36 -0800

    P24's finished

--- practice24/answer.js
+++ practice24/answer.js
@@ -1,3 +1,10 @@
+/** @function - 2nd Way: Function that takes in 2 multi-dimensional arrays and adds the values of their corresponding indexes.
+ * @name matrixAdd
+ * @param - {Array.<Number[]>} arr1 -  A multi-dimensional array.
+ * @param - {Array.<Number[]>} arr2 -  A multi-dimensional array.
+ * @type {[*]}
+ * @return {Array.<Number[]>} - Returns a multi-dimensional array.
+ */
 var arr1 = [[1, 2, 3], [3, 4, 5], [6, 7, 8]],
     arr2 = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
 
@@ -27,5 +34,38 @@ function matrixAdd (arr1,arr2) {
     }
     return output;
 }
-
 console.warn(matrixAdd(arr1, arr2));
+
+/** @function - 1st Way: Function that takes in 2 multi-dimensional arrays and adds the values of their corresponding indexes.
+ * @name matrixAdd
+ * @param - {Array.<Number[]>} arr1 -  A multi-dimensional array.
+ * @param - {Array.<Number[]>} arr2 -  A multi-dimensional array.
+ * @type {[*]}
+ * @return {Array.<Number[]>} - Returns a multi-dimensional array.
+ */
+// function matrixAdd(arrOne, arrTwo)
+// {
+//
+//     var i,
+//         j,
+//         addedVal,
+//         tempArr,
+//         output = [],
+//         length = arrOne.length;
+//
+//     for (i = 0; i < length; i++)
+//     {
+//         tempArr = [];
+//
+//         for (j = 0; j < length; j++)
+//         {
+//             addedVal = parseInt(arrOne[i][j]) + parseInt(arrTwo[i][j]);
+//             tempArr.push(addedVal);
+//         }
+//         output.push(tempArr);
+//     }
+//     return output;
+// }
+//
+// var result = matrixAdd(arr1, arr2);
+// console.log(result);
