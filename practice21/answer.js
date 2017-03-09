4e942c2a14bf9a791013eb06acd5f9799f509c65
John (Jef) Fisher <Fishe545@gmail.com>
2017-03-08 18:38:59 -0800

    added js files and JSDocumentation

--- practice21/answer.js
+++ practice21/answer.js
@@ -1,1 +1,14 @@
-answer.js
+/** @function - Function that simultaneously counts up on one variable, while counting down on another. The only code inside the for loop should be a console.log that lists both variables
+ * @name bipolarBearLoop
+ * @param {Number} incr - Number to increment.
+ * @param {Number} decr - Number to decrement.
+ * @return {Number[]} - Returns nothing but console logs.
+ */
+function bipolarBearLoop(incr, decr) {
+    var i = incr;
+    var j = decr;
+    for (i, j; i < j, j > i; i++, j--) {
+        console.log("i: " + i + " j: " + j);
+    }
+}
+bipolarBearLoop(24, 45);
