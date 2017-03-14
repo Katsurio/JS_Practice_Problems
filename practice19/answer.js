695c22bab48e6effffc23d03635ab472a2e76aa2
John (Jef) Fisher <Fishe545@gmail.com>
2017-03-13 19:01:39 -0700

    Problem 19's solved

--- practice19/answer.js
+++ practice19/answer.js
@@ -1,32 +1,31 @@
 var exArr =
     [
-        {name: 'John', age:42, gender:'M'},
-        {name: 'Jack', age:34, gender:'M'},
-        {name: 'Michael',age:40, gender: 'M'},
-        {name: 'Kendra',age:48, gender:'F'}
+        {name: 'John', age: 42, gender: 'M'},
+        {name: 'Jack', age: 34, gender: 'M'},
+        {name: 'Michael', age: 40, gender: 'M'},
+        {name: 'Kendra', age: 48, gender: 'F'}
     ];
 var keys =
     {
         name: "name",
         age: "age",
         gender: "gender"
-    }
+    };
 /** @function - Function that Takes a list of objects, and returns an array of those objects, sorted by their property 'name'.
  * @name findFactors
- * @param {Object[]} arr - Array to be sorted.
+ * @param {Object[]} arr - Array of objects to be sorted.
+ * @param {Object} sortPram - Key to use for sorting.
  * @return {Object[]} - Returns the sorted array.
  */
 function findFactors(arr, sortPram)
 {
-    var i,
-        iPlus1,
-        temp;
-    for(i = 0, iPlus1 = i + 1; i < arr.length; i++)
-    {
-        var tempBase = arr[i][sortPram],
-            tempNext = arr[iPlus1][sortPram];
+    var i, iPlus1, temp;
 
-        if (tempBase > tempNext)
+    for(i = 0; i < arr.length - 1; i++)
+    {
+        iPlus1 = i + 1;
+
+        if (arr[i][sortPram] > arr[iPlus1][sortPram])
         {
             temp = arr[i];
             arr[i] =  arr[iPlus1];
@@ -36,5 +35,3 @@ function findFactors(arr, sortPram)
     }
     return arr;
 }
-
-console.log(findFactors(exArr, keys.age));
