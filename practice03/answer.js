21695c1c8f4ed095e5f3aaa4c845e1e3bd6c4580
John (Jef) Fisher <Fishe545@gmail.com>
2017-08-28 23:51:42 -0700

    needs more work

--- practice03/answer.js
+++ practice03/answer.js
@@ -1,30 +1,55 @@
-var myarray = ['hello', 34, true, false, 'goodbye', 56, 12, '25', true];
+let myArray = ['hello', 34, true, false, 'goodbye', 56, 12, '25', true];
 /** @function - Function that sorts an array into a multidimensional array grouped by data type.
  * @name wunTooMullTea
  * @param {Array} arr - An array of anything.
  * @return {Array.<Array>} - Returns a multidimensional array grouped by data type.
  */
-function wunTooMullTea(arr)
-{
-    var temp = [[],[],[]];
+// function wunTooMullTea(arr)
+// {
+//     var temp = [[],[],[]];
+//
+//     for (var i = 0; i < arr.length; i++) {
+//         switch (typeof arr[i]) {
+//             case "boolean":
+//                 temp[2].push(arr[i]);
+//                 break;
+//             case "number":
+//                 temp[1].push(arr[i]);
+//                 break;
+//             case "string":
+//                 temp[0].push(arr[i]);
+//                 break;
+//             default:
+//                 alert("Something's not right");
+//         }
+//     }
+//     return temp;
+// }
 
-    for (var i = 0; i < arr.length; i++)
-    {
-        switch (typeof arr[i])
-        {
+
+const oneToMulti = (arr) => {
+    let temp = [[],[],[]];
+
+    return arr.map((word) => {
+        console.log("This: " + word);
+        if(typeof word === "boolean") {  }
+
+
+
+
+        switch (typeof word) {
             case "boolean":
-                temp[2].push(arr[i]);
+                temp[2].push(word);
                 break;
             case "number":
-                temp[1].push(arr[i]);
+                temp[1].push(word);
                 break;
             case "string":
-                temp[0].push(arr[i]);
+                temp[0].push(word);
                 break;
             default:
                 alert("Something's not right");
         }
-    }
-    return temp;
-}
-console.log(wunTooMullTea(myarray));
+    });
+};
+console.log(oneToMulti(myarray));
